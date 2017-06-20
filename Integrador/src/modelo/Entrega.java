046d1062dd6396d1b4132284b27aeb3cbb36e2cc
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-06-20 19:35:22 -0300

    Agregada la relacion con pedido, Linea

--- Integrador/src/modelo/Entrega.java
+++ Integrador/src/modelo/Entrega.java
@@ -9,16 +9,28 @@ import java.util.List;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
 /**
  *
  * @author Diego Raul Fernandez
  */
+@Entity
+@Table(name="entregas")
 public class Entrega implements Serializable{
+
+    @OneToOne
+    private Linea linea;
     @Id
     private Long idEntrega;
+    @Id //?
+    @ManyToOne
     private Pedido nroPedido;
+    @OneToMany(mappedBy = "entrega")
     private List<Linea> detalle;
+    @Temporal(javax.persistence.TemporalType.DATE)
     private Date fechaEntrega;
 }
