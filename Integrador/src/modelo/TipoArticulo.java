8f4fb940cc3c1ed7fb68b05e86867c0e0d621eb0
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-06-27 10:14:45 -0300

    Clase TipoArticulo.

---
+++ Integrador/src/modelo/TipoArticulo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ *
+ * @author Diego Raul Fernandez
+ */
+
+@Entity
+@Table(name="tipos_articulos")
+public class TipoArticulo implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long idTipoArticulo;
+    private String descripción;
+    @OneToMany(mappedBy = "tipoArticulo")
+    private List<Envase> envases;
+    @OneToOne
+    private Articulo articulo;
+
+    public TipoArticulo(){
+        this.envases = new ArrayList<>();
+    }
+    
+    public TipoArticulo(String descripcion){
+        this.descripción = descripcion;
+        this.envases = new ArrayList<>();
+    }
+    
+    public Long getIdTipoArticulo() {
+        return idTipoArticulo;
+    }
+
+    public void setIdTipoArticulo(Long idTipoArticulo) {
+        this.idTipoArticulo = idTipoArticulo;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+    
+    public List<Envase> getEnvases() {
+        return envases;
+    }
+
+    public void setEnvases(List<Envase> envases) {
+        this.envases = envases;
+    }
+}
