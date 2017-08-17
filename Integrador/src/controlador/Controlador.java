ae105dd2d30662a39ac76ae10ff9258975c8f65c
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-08-17 19:21:53 -0300

    Agregado los métodos nuevoCliente, nuevaEntrega y nuevoPedido

--- Integrador/src/controlador/Controlador.java
+++ Integrador/src/controlador/Controlador.java
@@ -21,9 +21,49 @@ public class Controlador {
         this.persistencia = p;
     }
     
+    //Controlador de clientes
     public List listarClientes(){
         return this.persistencia.buscarTodosOrdenadosPor(Cliente.class, 
                 Cliente_.nroCliente);
+        
+    }
+    
+    public void nuevoCliente(String razonSocial, String cuit, String calle, 
+            String numero, String localidad, String provincia){
+        Cliente cliente;
+        cliente = new Cliente(razonSocial, cuit, calle, numero, localidad, provincia);
+        this.persistencia.iniciarTransaccion();
+        try{
+            this.persistencia.insertar(cliente);
+            this.persistencia.confirmarTransaccion();
+        }catch(Exception ex){
+            this.persistencia.descartarTransaccion();
+        }
+    }
+    
+    //Controlador de Pedidos
+    public void nuevoPedido(String propietario, Date entregaInicial, 
+            Integer totalDeEntregas, char periodicidad){
+        this.persistencia.iniciarTransaccion();
+        try{
+            //DO SOMETHING
+            Cliente unPropietario;
+            unPropietario = this.persistencia.buscar(Cliente.class, propietario);
+            Pedido unPedido;
+            unPedido = new Pedido(unPropietario, entregaInicial,
+                    totalDeEntregas, periodicidad);
+            unPedido.setPropietario(unPropietario);
+            unPropietario.agregarPedido(unPedido);
+            this.persistencia.modificar(unPropietario);
+            this.persistencia.insertar(unPedido);
+            this.persistencia.confirmarTransaccion();
+        } catch(Exception ex){
+            this.persistencia.descartarTransaccion();
+        }
+    }
+    
+    //Controlador de Entrega
+    public void nuevaEntrega(){
         
     }
 }
