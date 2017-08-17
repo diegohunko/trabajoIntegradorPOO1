589cd535d5c4d0ba9ac1a3350b553528598a1dc2
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-08-17 19:20:14 -0300

    Sin medoficaciones sustanciales

--- Integrador/src/vistas/VentanaPrincipal.java
+++ Integrador/src/vistas/VentanaPrincipal.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package vistas;
-import persistencia.Persistencia;
+import vistas.articulos.VentanaArticulo;
+import vistas.envases.VentanaEnvase;
+import vistas.clientes.VentanaCliente;
+import vistas.pedidos.VentanaPedido;
 import controlador.Controlador;
 /**
  *
@@ -15,7 +18,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     /**
      * Creates new form Bienvenida
      */
-    private Controlador controlador;
+    private final Controlador controlador;
     public VentanaPrincipal(Controlador c) {
         this.controlador = c;
         initComponents();
@@ -30,35 +33,50 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        btnVentClientes = new javax.swing.JButton();
+        btnVentPedidos = new javax.swing.JButton();
+        btnVentEnvases = new javax.swing.JButton();
+        btnVentArticulos = new javax.swing.JButton();
+        btnCerrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Customers' Management");
-        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+        btnVentClientes.setText("Customers' Management");
+        btnVentClientes.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton1MouseClicked(evt);
+                btnVentClientesMouseClicked(evt);
             }
         });
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnVentClientes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnVentClientesActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Customers' orders' Management");
+        btnVentPedidos.setText("Customers' orders' Management");
+        btnVentPedidos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVentPedidosActionPerformed(evt);
+            }
+        });
 
-        jButton3.setText("Packaging");
+        btnVentEnvases.setText("Packaging");
+        btnVentEnvases.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVentEnvasesActionPerformed(evt);
+            }
+        });
 
-        jButton4.setText("Products");
+        btnVentArticulos.setText("Products");
+        btnVentArticulos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVentArticulosActionPerformed(evt);
+            }
+        });
 
-        jButton5.setText("Toquemos los tarros");
-        jButton5.setCursor(new java.awt.Cursor(java.awt.Cursor.CROSSHAIR_CURSOR));
-        jButton5.setName("btnCerrarAplicacion"); // NOI18N
+        btnCerrar.setText("Toquemos los tarros");
+        btnCerrar.setCursor(new java.awt.Cursor(java.awt.Cursor.CROSSHAIR_CURSOR));
+        btnCerrar.setName("btnCerrarAplicacion"); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -66,53 +84,72 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton5)
+                .addComponent(btnCerrar)
                 .addGap(24, 24, 24))
             .addGroup(layout.createSequentialGroup()
                 .addGap(136, 136, 136)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnVentEnvases, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnVentPedidos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnVentClientes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnVentArticulos, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(255, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(46, 46, 46)
-                .addComponent(jButton1)
+                .addComponent(btnVentClientes)
                 .addGap(18, 18, 18)
-                .addComponent(jButton2)
+                .addComponent(btnVentPedidos)
                 .addGap(18, 18, 18)
-                .addComponent(jButton3)
+                .addComponent(btnVentEnvases)
                 .addGap(18, 18, 18)
-                .addComponent(jButton4)
+                .addComponent(btnVentArticulos)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 177, Short.MAX_VALUE)
-                .addComponent(jButton5)
+                .addComponent(btnCerrar)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+    private void btnVentClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnVentClientesMouseClicked
         // TODO add your handling code here:
         
-    }//GEN-LAST:event_jButton1MouseClicked
+    }//GEN-LAST:event_btnVentClientesMouseClicked
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnVentClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentClientesActionPerformed
         // TODO add your handling code here:
-        VentanaCliente vc = new VentanaCliente();
+        VentanaCliente vc = new VentanaCliente(this.controlador, this);
         vc.setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnVentClientesActionPerformed
+
+    private void btnVentPedidosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentPedidosActionPerformed
+        // TODO add your handling code here:
+        VentanaPedido vPedido = new VentanaPedido(this.controlador, this);
+        vPedido.setVisible(true);
+    }//GEN-LAST:event_btnVentPedidosActionPerformed
+
+    private void btnVentEnvasesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentEnvasesActionPerformed
+        // TODO add your handling code here:
+        VentanaEnvase vEnv;
+        vEnv = new VentanaEnvase(this.controlador, this);
+        vEnv.setVisible(true);
+    }//GEN-LAST:event_btnVentEnvasesActionPerformed
+
+    private void btnVentArticulosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentArticulosActionPerformed
+        // TODO add your handling code here:
+        VentanaArticulo vArt = new VentanaArticulo(this.controlador, this);
+        vArt.setVisible(true);
+    }//GEN-LAST:event_btnVentArticulosActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton btnCerrar;
+    private javax.swing.JButton btnVentArticulos;
+    private javax.swing.JButton btnVentClientes;
+    private javax.swing.JButton btnVentEnvases;
+    private javax.swing.JButton btnVentPedidos;
     // End of variables declaration//GEN-END:variables
 }
