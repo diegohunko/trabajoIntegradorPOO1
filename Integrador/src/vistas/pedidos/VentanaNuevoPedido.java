1e43c681e302c1b5f5c5866f4ef0939b19c9c3fe
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-08-18 17:44:39 -0300

    lstEntregasMouseClicked, crea una ventana VentanaEntrega para agregar los artículos de la idem

--- Integrador/src/vistas/pedidos/VentanaNuevoPedido.java
+++ Integrador/src/vistas/pedidos/VentanaNuevoPedido.java
@@ -56,9 +56,6 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -93,6 +90,11 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
             public String getElementAt(int i) { return strings[i]; }
         });
         lstFechasEntregas.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        lstFechasEntregas.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lstFechasEntregasMouseClicked(evt);
+            }
+        });
         lstFechasEntregas.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
             public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
                 lstFechasEntregasValueChanged(evt);
@@ -111,12 +113,6 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
 
         jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel8.setText("Fechas de entrega para el pedido");
-
-        jTextField1.setText("jTextField1");
-
-        jTextField2.setText("jTextField2");
-
-        jLabel9.setText("jLabel9");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -158,13 +154,6 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 417, Short.MAX_VALUE)
                         .addGap(29, 29, 29))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(92, 92, 92))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
@@ -208,11 +197,6 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
                 .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(50, 50, 50)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel9))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -242,17 +226,24 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
 
     private void lstFechasEntregasValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_lstFechasEntregasValueChanged
         // TODO add your handling code here:
+        
+        
+    }//GEN-LAST:event_lstFechasEntregasValueChanged
+
+    private void lstFechasEntregasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lstFechasEntregasMouseClicked
+        // TODO add your handling code here:
         SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
         VentanaEntrega ve;
         try {
             ve = new VentanaEntrega(this.controlador, this, 
-                    formatoFecha.parse(this.lstFechasEntregas.getSelectedValue()));
+                    formatoFecha.parse(this.lstFechasEntregas.getSelectedValue()),
+                    1L); //? ver como pasar el pedido al que corresponde la entrega.
+            this.setVisible(false);
             ve.setVisible(true);
         } catch (ParseException ex) {
             Logger.getLogger(VentanaNuevoPedido.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-    }//GEN-LAST:event_lstFechasEntregasValueChanged
+    }//GEN-LAST:event_lstFechasEntregasMouseClicked
 
     private void formWindowClosing(java.awt.event.WindowEvent evt) {                                   
         // TODO add your handling code here:
@@ -275,10 +266,7 @@ public class VentanaNuevoPedido extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JList<String> lstFechasEntregas;
     private javax.swing.JTextField txtCuilPropietario;
     private javax.swing.JTextField txtTotalDeEntregas;
