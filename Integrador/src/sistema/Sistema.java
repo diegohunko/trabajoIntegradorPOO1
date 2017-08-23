7483abcec57a0b044f8cd6433d51acc046ede2b2
diegohunko <diegofernandezhunko@outlook.com.ar>
2017-08-23 20:28:09 -0300

    sentencia try catch agregada.

--- Integrador/src/sistema/Sistema.java
+++ Integrador/src/sistema/Sistema.java
@@ -10,6 +10,7 @@ import vistas.VentanaPrincipal;
 import persistencia.Persistencia;
 import controlador.Controlador;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Diego Raul Fernandez
@@ -17,13 +18,20 @@ import javax.swing.JFrame;
 public class Sistema {
     public static void main(String args[]){
         //TODO codigo aqui.
-        EntityManagerFactory emf;
-        emf = Persistence.createEntityManagerFactory("IntegradorPU");
-        Persistencia persistencia = new Persistencia(emf);
-        Controlador c = new Controlador(persistencia);
-        VentanaPrincipal miVentanaInicio;
-        miVentanaInicio = new VentanaPrincipal(c);
-        miVentanaInicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        miVentanaInicio.setVisible(true);
+        try{
+            EntityManagerFactory emf;
+            emf = Persistence.createEntityManagerFactory("IntegradorPU");
+            Persistencia persistencia = new Persistencia(emf);
+            Controlador c = new Controlador(persistencia);
+            VentanaPrincipal miVentanaInicio;
+            miVentanaInicio = new VentanaPrincipal(c);
+            miVentanaInicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            miVentanaInicio.setVisible(true);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, 
+                    e.getMessage(), 
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        
     }
 }
